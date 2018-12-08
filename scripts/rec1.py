531eab2ec24f4153dc172f8dd4d398fed0dc86ab
Unknown <ponur0kirill@gmail.com>
2018-12-08 21:27:49 +0300

    Сделаны первые два задания практики

--- scripts/rec1.py
+++ scripts/rec1.py
@@ -18,42 +18,42 @@ def prop():
     xlabel(r'$\nu, \text{кГц}$',fontsize=16)
 
 def task1():
-    name = path.abspath('..'+'\\rec\\experiment.xlsx')
+    name = path.abspath('..'+'\\rec\\rec.xlsx')
     df = pd.read_excel(name,sheet_name='1')
     df=df
     U=[]
     f=[]
     for i in df.index:
-        U.append(df['U(mV)'][i])
-        f.append(df['frec(kHz)'][i])
+        U.append(df['U,mV'][i])
+        f.append(df['freq, kHz'][i])
     plot(f,U,color= 'darkblue')
     plot(f,U,'r.')
     prop()
+    savefig(path.abspath('..'+'\\fig\\exp1.pdf'))
     show()
-
 def task2():
-    name = path.abspath('..'+'\\rec\\experiment.xlsx')
+    name = path.abspath('..'+'\\rec\\rec.xlsx')
     df = pd.read_excel(name,sheet_name='2')
     df=df
     U=[]
     f=[]
     for i in df.index:
-        U.append(df['U(mV)'][i])
-        f.append(df['frec(kHz)'][i])    
+        U.append(df['U,mV'][i])
+        f.append(df['freq,kHz'][i])    
     f=array(f)
     U=array(U)
     ff=f[~isnan(f)]
     UU=U[~isnan(U)]
-    g = interpolate.interp1d(ff,UU, kind='cubic')
+    g = interpolate.interp1d(ff,UU, kind='linear')
     x=linspace(ff[0],ff[-1],1000)
-    plot(x,g(x),color='darkblue')
-    plot(ff,UU,'r.')
+    plot(ff,UU,color='darkblue')
+    plot(ff,UU,'r.',markersize=3)
     prop()
+    savefig(path.abspath('..'+'\\fig\\exp2.pdf'))
     show()
-
 def task3(*args):
 
-    name = path.abspath('..'+'\\rec\\experiment.xlsx')
+    name = path.abspath('..'+'\\rec\\rec.xlsx')
     df = pd.read_excel(name,sheet_name='3')
     df=df
     U1=[]
@@ -64,31 +64,52 @@ def task3(*args):
     fon=[]
     Uoff=[]
     foff=[]
+    A=[]
+    DC=[]
     for i in df.index:
-        U1.append(df['u1(mv)'][i])
-        f1.append(df['frec1(kHz)'][i])
-        U2.append(df['u2(mv)'][i])
-        f2.append(df['frec2(kHz)'][i])
-        Uon.append(df['U(detec=on)(mv)'][i])
-        fon.append(df['frec(kHz)'][i])
-        Uoff.append(df['U(detec=off)(mv)'][i])
-        foff.append(df['frec(kHz)'][i])
+        A.append(df['AUin,mVpp'][i])
+        DC.append(df['DCout,mV'][i])    
+        f1.append(df['freq,kHz'][i])
+        U1.append(df['U1,mV'][i])
+        U2.append(df['U2,mV'][i])
+        f2.append(df['freq,kHz'][i])
+        Uon.append(df['on,mV'][i])
+        fon.append(df['freq1,kHz'][i])
+        Uoff.append(df['off,mV'][i])
+        foff.append(df['freq1,kHz'][i])
 
-    U=array([U1,U2,Uon,Uoff])
-    f=array([f1,f2,fon,foff])
-    for counter,value in enumerate(args):
-        figure(value)
-        prop()
-        ff=f[counter][~isnan(f[counter])]
-        UU=U[counter][~isnan(U[counter])]
-        g = interpolate.interp1d(ff,UU, kind='cubic')
-        x=linspace(ff[0],ff[-1],1000)
-        plot(x,g(x),color='darkblue')
-        plot(ff,UU,'r.')
 
+
+    figure('detec')
+    prop()
+    xlabel(r'$U_{\text{вх}}^{max}, \text{мВ}$')
+    ylabel(r'$U_{\text{вых}}$, \text{мВ}')
+    A=array(A)/2
+    A = A[~isnan(A)]
+    DC=array(DC)
+    DC=DC[~isnan(DC)]
+    f=polyfit(A,DC,1)
+    f=poly1d(f)
+    x=linspace(A[0],A[-1],100)
+    plot(x,f(x))
+    plot(A,DC,'r.')
+    savefig(path.abspath('..'+'\\fig\\exp3a.pdf'))
+
+    figure('12')
+    prop()
+    plot(f1,U1,'.-',label=r'$R_1C_1$')
+    plot(f2,U2,'.-',label=r'$R_2C_2$')
+    legend()
+    savefig(path.abspath('..'+'\\fig\\exp3b.pdf'))
+    figure('on/off')
+    prop()
+    plot(foff,Uoff,'.-',label=r'$\text{детектор выключен}$')
+    plot(fon,Uon,'.-',label=r'$\text{детектор включен}$')
+    legend()
+    savefig(path.abspath('..'+'\\fig\\exp3c.pdf'))
     show()
 ########################################################################
 
 task1()
 task2()
-task3(1,2,'on','off')
+# task3(1,2,'on','off')
